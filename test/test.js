62f9ccc05cf7e77161cff130105367be69379cc3
Jingwood <dujid0@gmail.com>
2020-02-08 09:34:38 +0900

    snap to another object

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@
 // MIT License (C) 2015-2020 Jingwood, unvell.com, all rights reserved.
 ////////////////////////////////////////////////////////////////////////////////
 
-import { Vec2 } from "@jingwood/graphics-math/dist/vec2";
+import { Vec2, Matrix3 } from "@jingwood/graphics-math";
 import { Keys } from "@jingwood/input-control";
 import { MathFunctions as _mf } from "@jingwood/graphics-math";
 import { Renderer2D } from "../src/render/renderer.js";
@@ -16,6 +16,25 @@ import { Ellipse } from "../src/shapes/ellipse";
 import { Rect } from "../src/types/rect";
 import { Object2D } from "../src/scene/object.js";
 import { DraggableObject } from "../src/shapes/draggable";
+import { Size } from "../src/types/size";
+
+if (!Object.prototype._t_foreach) {
+  Object.defineProperty(Object.prototype, "_t_foreach", {
+    value: function(iterator) {
+      if (this == null) {
+        throw "Not an object";
+      }
+      const _this = this || window;
+      for (const key in _this) {
+        if (_this.hasOwnProperty(key)) {
+          const ret = iterator.call(_this, key, _this[key]);
+          if (ret === false) break;
+        }
+      }
+    },
+    enumerable: false,
+  });
+}
 
 class TestRect extends DraggableObject {
   constructor() {
@@ -23,6 +42,8 @@ class TestRect extends DraggableObject {
   }
 
   draw(g) {
+    super.draw(g);
+
     g.drawRect(this.bbox.rect, this.style.strokeWidth, this.style.strokeColor, this.style.fillColor);
 
     if (g.options.debugMode) {
@@ -39,91 +60,195 @@ window.addEventListener("load", e => {
 
   let moveGuideLines = [];
 
-  scene.ondraw = g => {
-    // g.drawRoundRect({ x: 10, y: 10, width: 400, height: 40 }, 50, 6, "#aaa", "#eee");
-    // g.drawRoundRect({ x: 1000, y: 1000, width: 400, height: 40 }, 200, 2, "#aaa", "#eee");
+  const guideLines = {
+    x: [],
+    y: [],
 
-    for (const line of moveGuideLines) {
-      switch (line.type) {
-        case "left":
-        case "right":
-          g.drawLine({ x: line.start.x, y: 0 }, { x: line.start.x, y: renderer.renderSize.height }, "2", "blue");
-          break;
-      }
+    alternates: {
+      right: [],
+      left: [],
+      top: [],
+      bottom: [],
     }
   };
 
-  scene.onmousemove = (e) => {
-    scene.requestUpdateFrame();
+  scene.ondraw = g => {
+    g.drawRoundRect({ x: 10, y: 10, width: 400, height: 40 }, 50, 6, "#aaa", "#eee");
+
+    const maxx = renderer.renderSize.width, maxy = renderer.renderSize.height;
+
+    guideLines.alternates._t_foreach((srcType, alt) => {
+      if (alt.length > 0) {
+        const gl = alt[0];
+        const line = gl.l;
+
+        switch (gl.destType) {
+          case "left":
+            g.drawLine({ x: line.start.x, y: 0 }, { x: line.start.x, y: maxy }, "2", "blue");
+            break;
+              
+          case "right":
+            g.drawLine({ x: line.end.x, y: 0 }, { x: line.end.x, y: maxy }, "2", "blue");
+            break;
+ 
+          case "top":
+            g.drawLine({ x: 0, y: line.start.y }, { x: maxx, y: line.start.y }, "2", "red");
+            break;
+              
+          case "bottom":
+            g.drawLine({ x: 0, y: line.end.y }, { x: maxx, y: line.end.y }, "2", "red");
+            break;
+        }
+      }
+    });
   };
 
   const rect1 = new TestRect();
-  rect1.origin.set(100, 100);
+  rect1.origin.set(500, 100);
   rect1.size.set(400, 300);
-
-  Object2D.prototype.on("mousedown", function(e) {
-  });
 
   const r11 = new TestRect();
   r11.origin.set(100, 0);
   r11.style.fillColor = "lightgreen";
-  // r11.angle = 30;
+  r11.angle = 30;
   rect1.add(r11);
 
+  function findNearestObject(srcObj, p, srcType) {
+    function findEdgeOfObject(l, destType) {
+      const checkDistance = 30;
 
-  function findNearestObject(srcObj, p) {
-    moveGuideLines = [];
+      const dist = _mf.distancePointToLine2D(p, l);
+      if (dist < checkDistance) {
+        const gl = { srcType, destType, dist, l };
+        guideLines.alternates[srcType].push(gl);
+      }
+    }
 
     scene.eachObject(obj => {
-      if (obj === srcObj) return;
-
       const rect = new Rect(obj.wbbox.rect);
-      const checkDistance = 20;
       
-      let l, dist, gl;
-      l = rect.rightEdge;
-      dist = _mf.distancePointToLine2D(p, l);
-      if (dist < checkDistance) {
-        gl = { type: "right", dist, ...l };
-        moveGuideLines.push(gl);
+      if (srcType === "left" || srcType === "right") {
+        findEdgeOfObject(rect.rightEdge, "right");
+        findEdgeOfObject(rect.leftEdge, "left");
       }
+
+      if (srcType === "top" || srcType === "bottom") {
+        findEdgeOfObject(rect.topEdge, "top");
+        findEdgeOfObject(rect.bottomEdge, "bottom");
+      }
+    }, {
+      filter: obj => obj === srcObj
     });
+
   }
 
   rect1.drag = function(e) {
 
-    const targetOrigin = new Vec2(e.position).sub(this.dragOffset);
-    const targetTopLeft = targetOrigin.sub(rect1.size.v.mul(0.5));
-
-    findNearestObject(rect1, targetTopLeft);
-
-    moveGuideLines.sort((l1, l2) => l1.dist - l2.dist);
+    const obj = rect1;
 
     if (e.isKeyPressed(Keys.R)) {
-      rect1.angle += e.movement.x + e.movement.y;
+      obj.angle += e.movement.x + e.movement.y;
     } else if (e.isKeyPressed(Keys.S)) {
-      rect1.size.width += e.movement.x;
-      rect1.size.height += e.movement.y;
+      obj.size.width += e.movement.x;
+      obj.size.height += e.movement.y;
     } else {
 
-      if (moveGuideLines.length === 0) {
-        rect1.origin.set(targetOrigin);
-      } else {
-        const gl1 = moveGuideLines[0];
-        switch (gl1.type) {
-          case "right":
-            rect1.origin.set(gl1.start.x + rect1.size.width * 0.5, targetOrigin.y);
-            break;
-          
-          case "left":
-            rect1.origin.set(gl1.end.x + rect1.size.width * 0.5, targetOrigin.y);
-            break;
+      const targetOrigin = Vec2.sub(e.position, this.dragOffset);
+
+      moveGuideLines = [];
+      guideLines.alternates = {
+        right: [],
+        left: [],
+        top: [],
+        bottom: [],
+      };
+  
+      const hs = Size.toVector(obj.bbox.size).mul(0.5);
+      const rectp = new Rect(targetOrigin.sub(hs), obj.size).toPolygon();
+      rectp.transform(Matrix3.makeRotation(obj.angle));
+ 
+      findNearestObject(obj, rectp.points[0], "left");
+      findNearestObject(obj, rectp.points[1], "right");
+      findNearestObject(obj, rectp.points[0], "top");
+      findNearestObject(obj, rectp.points[1], "bottom");
+  
+      guideLines.alternates._t_foreach((_, alt) => {
+        if (alt.length > 0) {
+          alt.sort((l1, l2) => l1.dist - l2.dist);
+          moveGuideLines.push(alt[0]);
         }
+      });
+
+
+      if (moveGuideLines.length === 0) {
+        obj.origin.set(targetOrigin);
+      } else {
+
+        const hw = obj.size.width * 0.5, hh = obj.size.height * 0.5;
+
+        guideLines.alternates._t_foreach((srcType, alt) => {
+          if (alt.length > 0) {
+            const gl = alt[0];
+            const l = gl.l;
+
+            switch (srcType) {
+              case "left":
+                switch (gl.destType) {
+                  case "left":
+                    targetOrigin.x = l.start.x + hw;
+                    break;
+                  case "right":
+                    targetOrigin.x = l.end.x + hw;
+                    break;
+                }
+                break;
+          
+              case "right":
+                switch (gl.destType) {
+                  case "left":
+                    targetOrigin.x = l.start.x - hw;
+                    break;
+                  case "right":
+                    targetOrigin.x = l.end.x - hw;
+                    break;
+                }
+                break;
+           
+              case "top":
+                switch (gl.destType) {
+                  case "top":
+                    targetOrigin.y = l.start.y + hh;
+                    break;
+                  case "bottom":
+                    targetOrigin.y = l.end.y + hh;
+                    break;
+                }
+                break;
+            
+              case "bottom":
+                switch (gl.destType) {
+                  case "top":
+                    targetOrigin.y = l.start.y - hh;
+                    break;
+                  case "bottom":
+                    targetOrigin.y = l.end.y - hh;
+                    break;
+                }
+                break;
+            }
+          }
+        });
+
+        obj.origin.set(targetOrigin);
       }
     }
 
     scene.requestUpdateFrame();
   };
+
+  scene.on("enddrag", function(e) {
+    moveGuideLines = [];
+  });
 
   scene.add(rect1);
 
