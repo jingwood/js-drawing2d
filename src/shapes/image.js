7f96fdf30898edf7bc4a288762d380a789934e19
Jingwood <dujid0@gmail.com>
2020-05-17 12:00:58 +0900

    image clone support

--- src/shapes/image.js
+++ src/shapes/image.js
@@ -29,6 +29,16 @@ export class Image2D extends Rectangle2D {
   }
 
   drawSelf(g) {
-    g.drawImage(this._image, this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+    if (!this._image) {
+      console.warn("try to render an image object which doesn't have an valid image");
+    } else {
+      g.drawImage(this._image, this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+    }
+  }
+
+  clone() {
+    const newObj = super.clone();
+    newObj._image = this._image;
+    return newObj;
   }
 }
